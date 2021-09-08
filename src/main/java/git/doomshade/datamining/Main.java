6d74585f79843a1df5ea689686cd83115841f920
Jakub Šmrha <kuba.smrha@gmail.com>
2021-09-08 15:52:27 +0200

    initial commit

--- src/main/java/git/doomshade/datamining/Main.java
+++ src/main/java/git/doomshade/datamining/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         registerCommands();
         parseAndExecuteCommands(args);
+
     }
 
     private static void registerCommands() {
