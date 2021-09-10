909af2b7b876a4e7e59fa7afab62f290ca983556
Jakub Šmrha <kuba.smrha@gmail.com>
2021-09-10 19:28:49 +0200

    initial commit

--- src/main/java/git/doomshade/datamining/Main.java
+++ src/main/java/git/doomshade/datamining/Main.java
@@ -1,10 +1,15 @@
 package git.doomshade.datamining;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.squareup.okhttp.*;
 import git.doomshade.datamining.command.CommandManager;
 import git.doomshade.datamining.command.HelpCommand;
 import git.doomshade.datamining.command.TestCommand;
+import okio.BufferedSink;
 
-import static git.doomshade.datamining.command.CommandManager.parseAndExecuteCommands;
+import java.io.IOException;
+
 import static git.doomshade.datamining.command.CommandManager.registerCommand;
 
 /**
@@ -17,6 +22,28 @@ public class Main {
     public static void main(String[] args) {
         registerCommands();
         CommandManager.parseAndExecuteCommands(args);
+        /*OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://en.wikipedia.org/api/rest_v1/page/summary/Apple_Inc.")
+                .get()
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+            final String data = response.body().string();
+            final JsonObject jsonObject = JsonParser.parseString(data).getAsJsonObject();
+            System.out.println(jsonObject);
+            final String displayTitle = jsonObject.get("displaytitle").getAsString();
+            final JsonObject originalImage = jsonObject.get("originalimage").getAsJsonObject();
+            final String imageURL = originalImage.get("source").getAsString();
+            final String extractText = jsonObject.get("extract").getAsString();
+            System.out.println(displayTitle);
+            System.out.println(imageURL);
+            System.out.println(extractText);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
     }
 
     private static void registerCommands() {
