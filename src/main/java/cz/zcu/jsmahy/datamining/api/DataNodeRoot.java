a55355077e2ddb48d084c27821a89915ae508581
Jakub Šmrha <kuba.smrha@gmail.com>
2022-12-19 19:30:43 +0100

    Fix iterate method
    Signed-off-by: Jakub Šmrha <kuba.smrha@gmail.com>

--- src/main/java/cz/zcu/jsmahy/datamining/api/DataNodeRoot.java
+++ src/main/java/cz/zcu/jsmahy/datamining/api/DataNodeRoot.java
@@ -14,7 +14,7 @@ public interface DataNodeRoot<T> extends DataNode<T> {
     /**
      * <p>Iterates over the children of this root.</p>
      * <p>The first argument of the {@link BiConsumer} is the data node, the second argument is the breadth of the node in respect to the
-     * parent. In essence, the breadth represents the column width. For example:</p>
+     * parent. In essence, the breadth represents the column. For example:</p>
      * <ul>
      *     <li>(0) Karel IV</li>
      *      <ul>
@@ -28,7 +28,7 @@ public interface DataNodeRoot<T> extends DataNode<T> {
      * children of root.
      * </p>
      * <p>The first item to consume is the root which contains a null reference to data with depth of -1.</p>
-     * <p>The next items to consume are in DFS order - the nodes will be iterated in this order:</p>
+     * <p>The items to consume will be iterated in this order:</p>
      * <ul>
      *     <li>(0) Karel IV</li>
      *     <li>(1) Jan Lucembursky</li>
@@ -41,4 +41,14 @@ public interface DataNodeRoot<T> extends DataNode<T> {
      * @param biConsumer the bi-consumer
      */
     void iterate(BiConsumer<DataNode<T>, Integer> biConsumer);
+
+    /**
+     * Root has no data by default.
+     *
+     * @return nothing
+     *
+     * @throws UnsupportedOperationException if this method is called
+     */
+    @Override
+    T getData() throws UnsupportedOperationException;
 }
--- src/main/java/cz/zcu/jsmahy/datamining/api/DataNodeRootImpl.java
+++ src/main/java/cz/zcu/jsmahy/datamining/api/DataNodeRootImpl.java
@@ -1,6 +1,7 @@
 package cz.zcu.jsmahy.datamining.api;
 
 import javafx.collections.ObservableList;
+import lombok.NonNull;
 
 import java.util.function.BiConsumer;
 
@@ -11,18 +12,28 @@ public class DataNodeRootImpl<T> extends DataNodeImpl<T> implements DataNodeRoot
     public void iterate(BiConsumer<DataNode<T>, Integer> biConsumer) {
         final ObservableList<DataNode<T>> children = this.getChildren();
         if (!children.isEmpty()) {
-            iterate(biConsumer, -1, children.get(0));
+            iterate(biConsumer, 0, this);
         }
     }
 
+    @Override
+    public @NonNull T getData() {
+        throw new UnsupportedOperationException("Root has no data");
+    }
+
     private void iterate(BiConsumer<DataNode<T>, Integer> biConsumer, int depth, DataNode<T> dataNode) {
-        if (dataNode != null) {
+        if (dataNode == null) {
+            return;
+        }
+
+        if (!(dataNode instanceof DataNodeRoot<T>)) {
             biConsumer.accept(dataNode, depth);
-            if (dataNode.hasChildren()) {
-                depth++;
-                for (final DataNode<T> node : dataNode.getChildren()) {
-                    iterate(biConsumer, depth, node);
-                }
+        }
+
+        if (dataNode.hasChildren()) {
+            depth++;
+            for (final DataNode<T> node : dataNode.getChildren()) {
+                iterate(biConsumer, depth, node);
             }
         }
     }
