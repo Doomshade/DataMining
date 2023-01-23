11f60ad2ff5b2f086a1a13aaac75eff5565fe0d1
Jakub Šmrha <jsmahy@civ.zcu.cz>
2023-01-23 15:40:08 +0100

    feat: Use cache for cell value lookups

--- src/main/java/cz/zcu/jsmahy/datamining/query/handlers/OntologyPathPredicateInputResolver.java
+++ src/main/java/cz/zcu/jsmahy/datamining/query/handlers/OntologyPathPredicateInputResolver.java
@@ -1,5 +1,8 @@
 package cz.zcu.jsmahy.datamining.query.handlers;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.Weigher;
 import cz.zcu.jsmahy.datamining.api.*;
 import cz.zcu.jsmahy.datamining.query.RequestHandler;
 import javafx.application.Platform;
@@ -14,9 +17,7 @@ import org.apache.jena.rdf.model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public class OntologyPathPredicateInputResolver<T, R> implements BlockingAmbiguousInputResolver<T, R> {
@@ -35,12 +36,14 @@ public class OntologyPathPredicateInputResolver<T, R> implements BlockingAmbiguo
 
 
     private class DialogWrapper {
-        private static final Model EMPTY_MODEL = ModelFactory.createDefaultModel();
-
+        private static final Cache<String, String> modelCache = CacheBuilder.newBuilder()
+                                                                            .maximumWeight(5_242_880) // 5 MiB
+                                                                            .weigher((Weigher<String, String>) (key, value) -> key.length() + value.length())
+                                                                            .concurrencyLevel(20)
+                                                                            .build();
         private final Dialog<Property> dialog = new Dialog<>();
         private final TableView<Statement> content;
         private final DataNodeReferenceHolder<T> ref;
-        private final Map<String, String> modelCache = new HashMap<>();
 
         private DialogWrapper(final DataNodeReferenceHolder<T> ref, final StmtIterator candidateOntologyPathPredicates) {
             this.ref = ref;
@@ -49,7 +52,6 @@ public class OntologyPathPredicateInputResolver<T, R> implements BlockingAmbiguo
             this.content.getItems()
                         .addAll(candidateOntologyPathPredicates.toList());
             final TableColumn<Statement, String> propertyColumn = new TableColumn<>();
-
             propertyColumn.setCellValueFactory(this::cellValueFactoryCallback);
 
             final TableColumn<Statement, String> valueColumn = new TableColumn<>();
@@ -102,8 +104,9 @@ public class OntologyPathPredicateInputResolver<T, R> implements BlockingAmbiguo
                 return null;
             }
             final ReadOnlyObjectWrapper<String> observableValue = new ReadOnlyObjectWrapper<>();
-            if (modelCache.containsKey(uri)) {
-                observableValue.set(modelCache.get(uri));
+            final String cachedItem = modelCache.getIfPresent(uri);
+            if (cachedItem != null) {
+                observableValue.set(cachedItem);
                 return observableValue;
             }
             final Service<String> bgService = new Service<>() {
@@ -113,8 +116,9 @@ public class OntologyPathPredicateInputResolver<T, R> implements BlockingAmbiguo
                         @Override
                         protected String call() throws Exception {
                             synchronized (lock) {
-                                if (modelCache.containsKey(uri)) {
-                                    return modelCache.get(uri);
+                                final String cachedValue = modelCache.getIfPresent(uri);
+                                if (cachedValue != null) {
+                                    return cachedValue;
                                 }
                                 modelCache.put(uri, "");
                             }
